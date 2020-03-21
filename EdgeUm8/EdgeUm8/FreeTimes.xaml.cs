eab1449d0accc11dd0987de37bc78ab904dd78b0
patrikz1 <patrik.zetterblom@gmail.com>
2020-03-21 02:05:07 +0100

    Start på "lediga nu" krävs label med länk till avancerad samt namespace för att kunna göra combobox columns etc

--- EdgeUm8/EdgeUm8/FreeTimes.xaml.cs
+++ EdgeUm8/EdgeUm8/FreeTimes.xaml.cs
@@ -16,5 +16,11 @@ namespace EdgeUm8
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Profile());
+
+        }
     }
 }
