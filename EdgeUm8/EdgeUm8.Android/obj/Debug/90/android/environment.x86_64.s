31000541307d4ac870e99794ce8d423bb07718fc
patrikz1 <patrik.zetterblom@gmail.com>
2020-05-05 03:37:32 +0200

    update test profile pic

--- EdgeUm8/EdgeUm8.Android/obj/Debug/90/android/environment.x86_64.s
+++ EdgeUm8/EdgeUm8.Android/obj/Debug/90/android/environment.x86_64.s
@@ -69,7 +69,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"7b217632-3df8-40ce-be31-e5974651302b"
+	.asciz	"d52480b5-da5a-4abc-8e19-87b73250ea0e"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
