9158f5d60572f95504485b9b4e750bd3b6b9ccf3
patrikz1 <patrik.zetterblom@gmail.com>
2020-05-05 03:41:10 +0200

    well, last one didnt work, får köra på detta isf..

--- EdgeUm8/EdgeUm8.Android/Resources/Resource.designer.cs
+++ EdgeUm8/EdgeUm8.Android/Resources/Resource.designer.cs
@@ -5789,13 +5789,10 @@ namespace EdgeUm8.Droid
 			public const int notify_panel_notification_icon_bg = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int profile = 2131165300;
+			public const int tooltip_frame_dark = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int tooltip_frame_dark = 2131165301;
-			
-			// aapt resource value: 0x7F070076
-			public const int tooltip_frame_light = 2131165302;
+			public const int tooltip_frame_light = 2131165301;
 			
 			static Drawable()
 			{
@@ -6761,6 +6758,9 @@ namespace EdgeUm8.Droid
 			
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
+			
+			// aapt resource value: 0x7F0C0003
+			public const int profile = 2131492867;
 			
 			static Mipmap()
 			{
--- EdgeUm8/EdgeUm8.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ EdgeUm8/EdgeUm8.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -70,7 +70,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"d52480b5-da5a-4abc-8e19-87b73250ea0e"
+	.asciz	"34ede0ef-90ce-46d7-a097-e448ae3654c5"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
